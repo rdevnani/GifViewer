2bbde8f1fff5dd8a2e043790db0738d06f2cbc96
snehit <rohitdevnani@hotmail.com>
2017-11-26 16:02:32 +1100

    SwipeGestures and Transitions (Final)

--- GifViewer/DetailViewController.m
+++ GifViewer/DetailViewController.m
@@ -32,11 +32,29 @@
     
     dismiss.numberOfTapsRequired = 1;
     [self.view addGestureRecognizer:dismiss];
+    
+    UISwipeGestureRecognizer *swipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeToDismiss)];
+    swipe.direction = UISwipeGestureRecognizerDirectionRight;
+    [self.view addGestureRecognizer:swipe];
 }
 
 - (void)dismiss {
-    [self dismissViewControllerAnimated:YES completion:nil];
     
+    [UIView animateWithDuration:0.75 animations:^{
+        self.view.transform = CGAffineTransformMakeScale(0.01,0.01);
+        self.view.alpha = 0.0;
+    } completion:^(BOOL finished) {
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }];
+    
+}
+
+- (void) swipeToDismiss {
+    [UIView animateWithDuration:0.5 animations:^{
+        self.view.transform = CGAffineTransformMakeTranslation(400,0);
+    } completion:^(BOOL finished) {
+        [self dismissViewControllerAnimated:YES completion:nil];
+     }];
 }
 
 - (void)didReceiveMemoryWarning {
